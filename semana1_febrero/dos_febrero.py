54d19c19188e8580844ef00011b9f85cd040d1e4
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-03 04:48:34 +0000

    Mostrar el precio iva de un producto con valor de 670 y su precio final

---
+++ semana1_febrero/dos_febrero.py
@@ -0,0 +1,12 @@
+"""
+    Programa1
+    Nombre:Amayrani DB
+    Fecha: 2/02/23
+    Descripcion: Mostrar el precio iva de un producto con valor     de 670 y su precio final
+"""
+
+iva = 0.30
+precioproducto = 670
+precioiva = precioproducto * iva
+print("El precio del IVA es: ",precioiva)
+print("El precio final es: ", (precioiva + precioproducto) )
