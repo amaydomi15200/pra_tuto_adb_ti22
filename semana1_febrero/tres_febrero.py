cabaf1801ca73c625f994193ec60491a863d983a
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-04 05:10:56 +0000

    Calcular la longuitud de la hipotenusa de un triangulo rectangulo

--- semana1_febrero/tres_febrero.py
+++ semana1_febrero/tres_febrero.py
@@ -2,5 +2,11 @@
     Programa2
     Nombre:Amayrani DB
     Fecha: 3/02/23
-    Descripcion: Mostrar el precio iva de un producto con valor     de 670 y su precio final
+    Descripcion: Calcular la longuitud de la hipotenusa de un        triangulo rectangulo
 """
+
+puntoab = float(input("Escriba el valor de la longitud del vertice AB: "))  #se da a que escriban el valor del punto ab
+puntobc = float(input("Escriba el valor de la longitud del vertice BC: "))   # #se da a que escriban el valor del punto bc
+
+hipotenusa = (puntoab**2 + puntobc**2)  #se saca el valor de la hipotenusa sacando el cuadrado del punto ab y punto bc
+print("La longitud de la hipotenusa es: ",hipotenusa)  #se imprime en la pantalla la longitud de la hipotenusa
