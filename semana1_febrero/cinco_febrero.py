6d60f466031d54c5f1f48aa93aaf083e88537ae9
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-06 04:41:25 +0000

    Determinar si un estudiante aprobo un examen o lo reprobo

---
+++ semana1_febrero/cinco_febrero.py
@@ -0,0 +1,14 @@
+"""
+    Programa 4
+    Nombre:Amayrani DB
+    Fecha: 5/02/23
+    Descripcion: Determinar si un estudiante aprobo un examen o lo reprobo
+"""
+calif = float(input("Ingresa la calificacion de tu examen : "))  #pedira la calificacion del examen 
+
+if calif >= 7:  #dice si tu calificacion es mayor o igual a 7
+    print("APROBASTE EL EXAMEN") #va a imprir que eres aprobado en el examen
+
+else:  #da una condicion si no
+    print("REPTOBASTE EL EXAMEN")  #va aimprimir que reprobo el examen
+
