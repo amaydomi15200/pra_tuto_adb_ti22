ea742eee460b83ce73cd6bf02c6e3b02a37dbe99
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-11 05:02:36 +0000

    hacer una suma de dos numeros utilizando la funcion def

---
+++ semana2_febrero/diez_febrero.py
@@ -0,0 +1,15 @@
+"""
+    Programa 8
+    Nombre:Amayrani DB
+    Fecha: 10/02/23
+    Descripcion: hacer una suma de dos numeros utilizando la funcion def
+"""
+
+def suma(num1, num2):  #define las variables de la suma
+    print(num1 + num2)  #imprime la suma de dos numeros
+
+suma(6,4)  #llamada a los numero
+
+suma(18,100)
+
+suma(567,250)
