043d26c9c4dc7d997627c9aaaef1eeb7b37de5ac
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-13 03:33:53 +0000

    Operadores in situ

---
+++ semana2_febrero/doce_febrero.py
@@ -0,0 +1,11 @@
+"""
+    Programa 10
+    Nombre:Amayrani DB
+    Fecha: 12/02/23
+    Descripcion: Operadores in situ 
+"""
+x = 2 #se le da valor a x en este caso 2
+print(x)  #imprime el valor de x
+
+x += 3  #a "x" se le suma tres
+print(x)  #impeime el valor de x sumandole los tres
