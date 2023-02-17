1d332b0fa1c4f97e1ac9a94ebe57132baf148867
amaydomi15200 <122801344+amaydomi15200@users.noreply.github.com>
2023-02-17 02:52:13 +0000

    decide si es falso a lo que le manda

---
+++ semana3_febrero/febrero16.py
@@ -0,0 +1,15 @@
+"""
+    Programa13
+    Nombre:Amayrani DB
+    Fecha: 16/02/23
+    Descripcion:decide si es falso a lo que le manda
+"""
+boleano = True
+print(boleano)
+True
+
+print(2 == 3)
+False
+
+print("amay" == "amay")
+True
